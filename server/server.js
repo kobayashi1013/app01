f77630f196eb4478aac6500e499202a96d15f847
kobayashi <c612101397@tokushima-u.ac.jp>
2024-06-13 18:11:10 +0900

    push

--- server/server.js
+++ server/server.js
@@ -1,26 +1,27 @@
 const express = require("express");
 const path = require("path");
-
 const app = express();
 const port = 8080;
+
 const dirname = __dirname + "/..";
+let prevMessages = []; //過去のメッセージ
 
 //アプリ設定
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(dirname, "client")));
+app.set("view engine", "ejs");
+app.set("views", path.join(dirname, "client"));
 
 //サーバー処理
 app.get("/", (req, res) => {
-    res.sendFile(path.join(dirname, "client", "index.html"));
+    res.render("index", { prevMessages });
 })
 
 app.post("/", (req, res) => {
+    
+    //過去の書き込みの更新
     const { name, message } = req.body;
-
-    console.log(name);
-    console.log(message);
-
-    res.sendFile(path.join(dirname, "client", "index.html"));
+    prevMessages.push({ name, message });
+    res.redirect("/");
 });
 
 //Server Run
