07e38a3b6150190903cf26af8b88278ec51363e0
kobayashi <c612101397@tokushima-u.ac.jp>
2024-10-16 14:31:44 +0900

    アプリ名称の変更

---
+++ src/main/java/kobayashi/com/app01/constant/ModelKey.java
@@ -0,0 +1,6 @@
+package kobayashi.com.app01.constant;
+
+public class ModelKey {
+    
+    public static final String APPLICATION_NAME = "applicationName";
+}
--- src/main/java/kobayashi/com/app01/controller/SigninController.java
+++ src/main/java/kobayashi/com/app01/controller/SigninController.java
@@ -1,18 +1,24 @@
 package kobayashi.com.app01.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import kobayashi.com.app01.constant.ModelKey;
 import kobayashi.com.app01.constant.Url;
 import kobayashi.com.app01.form.SigninForm;
 
 @Controller
 public class SigninController {
     
+    @Value("${application.name}")
+    private String APPLICATION_NAME;
+
     @GetMapping(Url.SIGNIN_URL)
     public String view(Model model, SigninForm form) {
 
+        model.addAttribute(ModelKey.APPLICATION_NAME, APPLICATION_NAME);
         return Url.SIGNIN;
     }
 }
